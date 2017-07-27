0460456e9a00986283fd0377ad7eff1e091281fd
Dan Meyer <somesing9@gmail.com>
2017-07-27 13:02:58 -0400

    Updated files

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendData = [
+ 	{
+ 		name: "Google",
+ 		photo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+ 		scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+ 	},
+ 	{
+ 		name: "Microsoft",
+ 		photo: "https://assets.onestore.ms/cdnfiles/external/uhf/long/9a49a7e9d8e881327e81b9eb43dabc01de70a9bb/images/microsoft-gray.png",
+ 		scores: ["1", "1", "1", "1", "2", "1", "2", "3", "4", "5"]
+ 	},
+];
+
+module.exports = friendData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,68 @@
+var friendData = require("../data/friends.js");
+
+module.exports = function(app) {
+  // GET /api/friends route, display a JSON of all possible friends
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  // POST /api/friends, handle incoming surveys, also handle compatibility logic
+  app.post("/api/friends", function(req, res) {
+    console.log("API POST");
+    // If less than 2 people in database, return false
+    if (friendData.length < 1) {
+      console.log(req.body);
+      friendData.push(req.body);
+      // console.log(friendData);
+      res.json(false);
+    } else {
+      var newFriend = req.body;
+      var bestMatchScore = 0;
+      var bestMatchIndex = 0;
+      var currentScore = 0;
+      var nfScores = newFriend.scores;
+      var cfScores = [];
+
+      for (var i = 0; i < friendData.length; i++) {
+        cfScores = friendData[i].scores;
+        currentScore = 0;
+        for (var j = 0; j < newFriend.scores.length; j++) {
+
+          var cfScores = friendData[i].scores;
+          if (parseInt(nfScores[j]) != parseInt(cfScores[j])) {
+            var difference = parseInt(nfScores[j]) - parseInt(cfScores[j]);
+            console.log(difference);
+            if (difference < 0) {
+              difference = difference * -1;
+            }
+            currentScore += difference;
+          }
+
+          // console.log(friendData[i].name + " Q:" + j + " " + friendData[i].scores[j]);
+        }
+        console.log("Current score" + currentScore);
+        console.log("bestMatchIndex" + bestMatchIndex);
+
+        if (i === 0) {
+        	bestMatchScore = currentScore;
+        }
+
+        if (currentScore < bestMatchScore) {
+        	console.log("Best" + bestMatchScore + " currentScore " + currentScore);
+          bestMatchScore = currentScore;
+          bestMatchIndex = i;
+        }
+        console.log("bestMatchIndex" + bestMatchIndex);
+        console.log(friendData[i].name + " difference is: " + currentScore);
+        // friendData[i]
+      }
+      // console.log("New Friend = " + JSON.stringify(newFriend, null, 2));
+      console.log("Best Match = " + JSON.stringify(friendData[bestMatchIndex], null, 2) + " Score: " + bestMatchScore);
+      friendData.push(req.body);
+      // console.log(JSON.stringify(friendData, null, 2));
+      res.json(friendData[bestMatchIndex]);
+    }
+    // If more than 2 process logic to find the best match
+    // Display the result in a modal popup, with name and picture
+  });
+}
